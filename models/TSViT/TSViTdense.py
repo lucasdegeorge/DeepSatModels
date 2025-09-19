a453de50548f9e4aa5f10b03f247beb110b62199
lucasdegeorge <lucas.degeorge@outlook.fr>
2025-09-19 09:49:51 +0200

    PoM support added

--- models/TSViT/TSViTdense.py
+++ models/TSViT/TSViTdense.py
@@ -3,9 +3,20 @@ from torch import nn
 import torch.nn.functional as F
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
-from models.TSViT.module import Attention, PreNorm, FeedForward
 import numpy as np
+import sys
+import os
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(os.path.dirname(DIR))
+sys.path.append(os.path.dirname(os.path.dirname(DIR)))
+
+
 from utils.config_files_utils import get_params_values
+from models.TSViT.module import Attention, PreNorm, FeedForward
+from models.TSViT.pom_module import PoM
+
 
 
 class Transformer(nn.Module):
@@ -16,6 +27,24 @@ class Transformer(nn.Module):
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
                 PreNorm(dim, Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
+            ]))
+
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return self.norm(x)
+    
+    
+class PoMformer(nn.Module):
+    def __init__(self, dim, depth, degree, expand, mlp_dim, dropout=0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        self.norm = nn.LayerNorm(dim)
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, PoM(dim, degree=degree, expand=expand)),
                 PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
             ]))
 
@@ -436,6 +465,87 @@ class TSViT(nn.Module):
         x = x.reshape(B, H, W, self.num_classes)
         x = x.permute(0, 3, 1, 2)
         return x
+    
+    
+class TSViPoM(nn.Module):
+    """
+    Temporal-Spatial ViT5 (used in main results, section 4.3)
+    For improved training speed, this implementation uses a (365 x dim) temporal position encodings indexed for
+    each day of the year. Use TSViT_lookup for a slower, yet more general implementation of lookup position encodings
+    """
+    def __init__(self, model_config):
+        super().__init__()
+        self.image_size = model_config['img_res']
+        self.patch_size = model_config['patch_size']
+        self.num_patches_1d = self.image_size//self.patch_size
+        self.num_classes = model_config['num_classes']
+        self.num_frames = model_config['max_seq_len']
+        self.dim = model_config['dim']
+        if 'temporal_depth' in model_config:
+            self.temporal_depth = model_config['temporal_depth']
+        else:
+            self.temporal_depth = model_config['depth']
+        if 'spatial_depth' in model_config:
+            self.spatial_depth = model_config['spatial_depth']
+        else:
+            self.spatial_depth = model_config['depth']
+
+        self.degree = model_config['degree']
+        self.expand = model_config['expand']
+        
+        self.dropout = model_config['dropout']
+        self.emb_dropout = model_config['emb_dropout']
+        self.pool = model_config['pool']
+        self.scale_dim = model_config['scale_dim']
+        assert self.pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+        num_patches = self.num_patches_1d ** 2
+        patch_dim = (model_config['num_channels'] - 1) * self.patch_size ** 2  # -1 is set to exclude time feature
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b t c (h p1) (w p2) -> (b h w) t (p1 p2 c)', p1=self.patch_size, p2=self.patch_size),
+            nn.Linear(patch_dim, self.dim),)
+        self.to_temporal_embedding_input = nn.Linear(366, self.dim)
+        self.temporal_token = nn.Parameter(torch.randn(1, self.num_classes, self.dim))
+        self.temporal_transformer = PoMformer(
+            self.dim, self.temporal_depth, self.degree, self.expand, self.dim * self.scale_dim, self.dropout
+        )
+        self.space_pos_embedding = nn.Parameter(torch.randn(1, num_patches, self.dim))
+        self.space_transformer = PoMformer(
+            self.dim, self.spatial_depth, self.degree, self.expand, self.dim * self.scale_dim, self.dropout
+        )
+        self.dropout = nn.Dropout(self.emb_dropout)
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(self.dim),
+            nn.Linear(self.dim, self.patch_size**2)
+        )
+
+    def forward(self, x):
+        x = x.permute(0, 1, 4, 2, 3)
+        B, T, C, H, W = x.shape
+
+        xt = x[:, :, -1, 0, 0]
+        x = x[:, :, :-1]
+        xt = (xt * 365.0001).to(torch.int64)
+        xt = F.one_hot(xt, num_classes=366).to(torch.float32)
+
+        xt = xt.reshape(-1, 366)
+        temporal_pos_embedding = self.to_temporal_embedding_input(xt).reshape(B, T, self.dim)
+        x = self.to_patch_embedding(x)
+        x = x.reshape(B, -1, T, self.dim)
+        x += temporal_pos_embedding.unsqueeze(1)
+        x = x.reshape(-1, T, self.dim)
+        cls_temporal_tokens = repeat(self.temporal_token, '() N d -> b N d', b=B * self.num_patches_1d ** 2)
+        x = torch.cat((cls_temporal_tokens, x), dim=1)
+        x = self.temporal_transformer(x)
+        x = x[:, :self.num_classes]
+        x = x.reshape(B, self.num_patches_1d**2, self.num_classes, self.dim).permute(0, 2, 1, 3).reshape(B*self.num_classes, self.num_patches_1d**2, self.dim)
+        x += self.space_pos_embedding#[:, :, :(n + 1)]
+        x = self.dropout(x)
+        x = self.space_transformer(x)
+        x = self.mlp_head(x.reshape(-1, self.dim))
+        x = x.reshape(B, self.num_classes, self.num_patches_1d**2, self.patch_size**2).permute(0, 2, 3, 1)
+        x = x.reshape(B, H, W, self.num_classes)
+        x = x.permute(0, 3, 1, 2)
+        return x
 
 
 class TSViT_lookup(nn.Module):
@@ -562,19 +672,29 @@ class TSViT_lookup(nn.Module):
 
 if __name__ == "__main__":
     res = 24
-    model_config = {'img_res': res, 'patch_size': 3, 'patch_size_time': 1, 'patch_time': 4, 'num_classes': 20,
-                    'max_seq_len': 16, 'dim': 128, 'temporal_depth': 6, 'spatial_depth': 2,
-                    'heads': 4, 'pool': 'cls', 'num_channels': 14, 'dim_head': 64, 'dropout': 0., 'emb_dropout': 0.,
-                    'scale_dim': 4, 'depth': 4}
+    model_config = {
+        'img_res': res, 'patch_size': 3, 'patch_size_time': 1, 'patch_time': 4, 'num_classes': 20,
+        'max_seq_len': 16, 'dim': 128, 'temporal_depth': 6, 'spatial_depth': 2,
+        'heads': 4, 'pool': 'cls', 'num_channels': 14, 'dim_head': 64, 'dropout': 0., 'emb_dropout': 0.,
+        'scale_dim': 4, 'depth': 4
+    }
+    pom_config = {
+        'img_res': res, 'patch_size': 3, 'patch_size_time': 1, 'patch_time': 4, 'num_classes': 20,
+        'max_seq_len': 16, 'dim': 64, 'temporal_depth': 6, 'spatial_depth': 2,
+        'degree': 2, 'pool': 'cls', 'num_channels': 14, 'expand': 4, 'dropout': 0., 'emb_dropout': 0.,
+        'scale_dim': 4, 'depth': 4
+
+    }
     train_config = {'dataset': "psetae_repl_2018_100_3", 'label_map': "labels_20k2k", 'max_seq_len': 16, 'batch_size': 5,
                     'extra_data': [], 'num_workers': 4}
 
-    x = torch.rand((3, 16, res, res, 14))
+    x = torch.rand((3, 16, res, res, 14)).cuda()
 
-    # model = TViT(model_config).cuda()
+    model = TViT(model_config).cuda()
+    # model = TSViPoM(pom_config).cuda()
     # model = STViT(model_config)#.cuda()
     # model = TSViT_global_attention_spatial_encoder(model_config)#.cuda()
-    model = TSViT_single_token(model_config)
+    # model = TSViT_single_token(model_config)
 
     parameters = filter(lambda p: p.requires_grad, model.parameters())
     parameters = sum([np.prod(p.size()) for p in parameters]) / 1_000_000
@@ -583,4 +703,4 @@ if __name__ == "__main__":
     out = model(x)
 
     # torch.norm(cls, dim=2).shape
-    print("Shape of out :", out.shape)  # [B, num_classes]
+    print("Shape of out :", out.shape)  # [B, num_classes]ı
